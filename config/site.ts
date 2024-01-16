9ef11d680b7d815a710ffb0c88712d8f97997ab2
lexandfr <alexfloresrayme@gmail.com>
2024-01-16 11:58:46 -0500

    Update favicon and add site configuration

---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Taskiflow",
+  description: "Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Taskiflow.",
+}
