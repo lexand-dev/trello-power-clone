4325ffaaeb820669f8b55189c876802ebc46089a
lexandfr <alexfloresrayme@gmail.com>
2024-01-25 09:08:49 -0500

    Add form, board components and sonner toast

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,17 @@
-const OrganizationIdPage = () => {
-  return <div>organizatiion</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/Info";
+import { BoardList } from "./_components/board-list";
+
+const OrganizationIdPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info isPro />
+      <Separator className="my-4" />
+      <div>
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
