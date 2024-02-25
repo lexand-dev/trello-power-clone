4133de075cab5996faa8cfd412c434711cb0653f
lexandfr <alexfloresrayme@gmail.com>
2024-02-25 06:03:35 -0500

    Agrega proveedores de modal y componentes

---
+++ components/modals/card-modal/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { useQuery } from "@tanstack/react-query";
+
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+import { Actions } from "./action";
+import { Description } from "./description";
+import { Header } from "./header";
+
+export const CardModal = () => {
+  const id = useCardModal((state) => state.id);
+  const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
+
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+    enabled: isOpen,
+  });
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+              {/* {!auditLogsData
+                ? <Activity.Skeleton />
+                : <Activity items={auditLogsData} />
+              } */}
+            </div>
+          </div>
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
